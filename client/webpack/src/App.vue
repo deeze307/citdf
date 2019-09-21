4849a5e15f8da89b2fc651c01804c7ae2349b693
Diego Maidana <deeze.designs@gmail.com>
2019-09-21 18:39:43 -0300

    menú móvil

--- client/webpack/src/App.vue
+++ client/webpack/src/App.vue
@@ -1,13 +1,151 @@
 <template>
   <v-app>
-      <!-- MENU DE NAVEGACION !-->
+      <!-- MENU DE NAVEGACION MOBILE!-->
+      <v-app-bar
+        app
+        v-if="isMobile"
+        :color="menu_toolbar.header_color"
+        dark
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"><v-icon>menu</v-icon></v-app-bar-nav-icon>
+        <a href="/"><img src="@/assets/citdf-logo-dark.png" alt="citdf"></a>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <!-- Login / Logout -->
+          <div v-if="!logged">
+            <v-btn @click=" loginDialog = true" outlined color="white" small rounded :loading="loginWorking">
+              Ingresar
+            </v-btn>
+            <v-dialog v-model="loginDialog" max-width="400">
+              <login_api />
+            </v-dialog>
+          </div>
+          <!-- <v-container v-if="logged"> -->
+          <v-menu
+            v-if="logged"
+            transition="slide-y-transition"
+            offset-y
+            bottom
+            left
+            light
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on" :color="menu_toolbar.header_color" small rounded>
+                Hola {{(_.has(user.user,'firstName')) ? user.user.firstName : ''}}
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in userItems"
+                :key="i"
+                @click="callToAction(item.path)"
+                v-if="checkAdmin(item)"
+              >
+                <v-list-item-title v-if="item.title">{{ item.title }}</v-list-item-title>
+              </v-list-item>
+              <v-divider></v-divider>
+              <v-list-item @click="logout()">
+                <v-list-item-title ><v-icon left >power_settings_new</v-icon>Cerrar Sesion</v-list-item-title>
+              </v-list-item>
+
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        :color="menu_toolbar.header_color"
+        left
+        absolute
+        dark
+        temporary
+      >
+        <v-menu
+          transition="slide-y-transition"
+          offset-y
+          bottom
+          open-on-hover
+          dark
+          v-for="item in menu_toolbar.items"
+          :key="item.order"
+        >
+          <template v-slot:activator="{ on }"  >
+            <!-- Si los elementos del menu no tienen hijos -->
+            <v-list>
+              <v-list-item
+                class="my-0 tile"
+                v-if="!item.children"
+                :to="formatRoute(item.object_slug)"
+              >
+              {{item.title}}
+              </v-list-item>
+
+              <!-- Si los elementos del menu tienen hijos -->
+              <v-btn
+                v-if="item.children"
+                v-on="on"
+                small
+                text
+                class="mx-0"
+              >
+              {{item.title}}
+              </v-btn>
+            </v-list>
+          </template>
+          <v-list v-if="item.children">
+            <v-list-item
+              class="tile"
+              v-for="(child) in item.children"
+              :key="child.order"
+              :to="subMenu(child)"
+            >
+              <v-list-item-title v-if="!child.children" >{{ child.title }}</v-list-item-title>
+
+              <v-menu
+                transition="slide-y-transition"
+                offset-x
+                right
+                open-on-hover
+                dark
+                v-for="subChild in child.children"
+                :key="subChild.order"
+              >
+                <template v-slot:activator="{ on2 }" >
+                  <!-- Si los elementos del menu tienen hijos -->
+                  <v-btn
+                    v-on="on2"
+                    x-small
+                    text
+                  >
+                  <!-- {{subChild.title}} -->
+                  </v-btn>
+                </template>
+                <v-list v-if="subChild.children">
+                  <v-list-item
+                    class="tile"
+                    v-for="(subSubChild) in subChild.children"
+                    :key="subSubChild.order"
+                    :to="subSubChild.object_slug"
+                  >
+                    <v-list-item-title v-if="!subSubChild.children" >{{ subSubChild.title }}</v-list-item-title>
+                  </v-list-item>
+
+                </v-list>
+              </v-menu>
+            </v-list-item>
+
+          </v-list>
+        </v-menu>
+      </v-navigation-drawer>
       
       <!-- FIN MENU DE NAVEGACION !-->
-      <!-- Menu toolbar !-->
-      <v-app-bar app :color="menu_toolbar.header_color" dark>
+      <!-- Menu toolbar WEB !-->
+      <v-app-bar v-if="!isMobile" app :color="menu_toolbar.header_color" dark >
         <a href="/"><img src="@/assets/citdf-logo-dark.png" alt="citdf"></a>
         <!-- <v-toolbar-title v-text="menu_toolbar.title" ></v-toolbar-title> -->
         <v-spacer></v-spacer>
+        
           <v-toolbar-items>
 
             <v-menu
@@ -78,16 +216,6 @@
                         :to="subSubChild.object_slug"
                       >
                         <v-list-item-title v-if="!subSubChild.children" >{{ subSubChild.title }}</v-list-item-title>
-
-                      <!-- Si los elementos del menu tienen hijos -->
-                      <!-- <v-btn
-                        v-if="item.children"
-                        v-on="on"
-                        x-small
-                        text
-                      >
-                      {{item.title}}
-                      </v-btn> -->
                       </v-list-item>
 
                     </v-list>
@@ -222,7 +350,9 @@
     components :{ MenuSidebar, UserSidebar, login_api},
     data () {
       return {
+        isMobile:true,
         drawer: false,
+        group:null,
         toolbar_disabled:true,
         maintenanceMode: false,
         loginWorking:false,
@@ -252,6 +382,9 @@
       ]
       }
     },
+    mounted: function() {
+      this.onResize();
+    },
     computed: {
       menu_toolbar() {
         return store.state.menu_toolbar;
@@ -271,9 +404,11 @@
           store.state.login_api.showDialog = newValue;
         }
       },
-      
     },
     watch:{
+      group() {
+        this.drawer = false
+      },
       loginDialog(){},
       user(){},
       logged(state){
@@ -329,7 +464,14 @@
       },
       formatRoute(route){
         return "/"+route;
-      }
+      },
+      onResize(){
+        if(window.innerWidth <= 480){
+          this.isMobile = true;
+        }else{
+          this.isMobile = false;
+        }
+      },
     },
     name: 'App'
   }
