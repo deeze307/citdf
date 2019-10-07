6b062f0e51bcfc52151ec931f08cf6cbfc0f629d
Diego Maidana <deeze.designs@gmail.com>
2019-10-07 15:06:50 -0300

    conexiones en local

--- client/webpack/config/prod.env.js
+++ client/webpack/config/prod.env.js
@@ -1,5 +1,5 @@
 let opt = {};
-let ingress = process.env.CITDF_WPAPI || 'http://api-deeze.tk/citdf/wordpress/wp-json';
+let ingress = process.env.CITDF_WPAPI || 'http://localhost/citdf/wordpress/wp-json';
 
 opt.NODE_ENV = '"production"';
 opt.CITDF_WPAPI = JSON.stringify(ingress);
