4ee2c75f2cca47a4415f2fc3a7259ede88773b7c
Diego Maidana <deeze.designs@gmail.com>
2020-04-20 02:29:31 -0300

    renombra tabla slider_img a sliders

--- server/models/slider_img.js
+++ server/models/slider_img.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const slider_img = sequelize.define('slider_img', {
+  const sliders = sequelize.define('sliders', {
     display_name: DataTypes.STRING,
     url: DataTypes.STRING,
     enabled: DataTypes.INTEGER
   }, {timestamps:false});
-  slider_img.associate = function(models) {
+  sliders.associate = function(models) {
     // associations can be defined here
   };
-  return slider_img;
+  return sliders;
 };
