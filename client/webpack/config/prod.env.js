69f765f22da3f300e39ca1a3f7032f485a010e86
Diego Maidana <dmaidana01@gmail.com>
2020-11-24 11:31:24 -0300

    build conf modificaciones para prod

--- client/webpack/config/prod.env.js
+++ client/webpack/config/prod.env.js
@@ -4,5 +4,6 @@ let ingress = process.env.CITDF_WPAPI || '"http://api-deeze.tk/citdf/wordpress/w
 console.log("ingress: "+ingress);
 opt.NODE_ENV = '"production"';
 opt.CITDF_WPAPI = JSON.stringify(ingress);
+opt.CITDF_API = '"http://api-deeze.tk:3031"'
 
 module.exports = opt;
